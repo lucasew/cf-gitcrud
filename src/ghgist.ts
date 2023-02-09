864d13d198c90c1b92e291702e20189bc6b2a0db
lucasew <lucas59356@gmail.com>
2023-02-09 12:51:38 -0300

    ghgist: cache workaround to overcome github ttl issue
    Signed-off-by: lucasew <lucas59356@gmail.com>

--- src/ghgist.ts
+++ src/ghgist.ts
@@ -41,17 +41,24 @@ export async function peek(gistId: string) {
 	const res = await update(gistId, {
 		".gitcrud": "This is a placeholder file because it's impossible to create empty gists"
 	})
-	return Response(res.statusText, res.status)
+	return new Response(res.statusText, {status: res.status})
 }
 
 export async function get(gistId: string, filename: string) {
 	const rawGistURL = await fetch(`https://gist.github.com/${gistId}`)
-	console.log(rawGistURL.url)
-	const res = await fetch(`${rawGistURL.url}/raw/${filename}`)
-	if (res.ok) {
-		return new Response(res.body)
+	const cachedResponse = await caches.default.match(`https://ghgist.local/${gistId}/${filename}`)
+	if (cachedResponse) {
+		return cachedResponse
 	} else {
-		return new Response(res.statusText, { status: res.status })
+		const res = await fetch(`${rawGistURL.url}/raw/${filename}`)
+		if (res.ok) {
+			let response = new Response(res.clone().body)
+			response.headers.append('Cache-Control', `s-maxage=300`)
+			await caches.default.put(`https://ghgist.local/${gistId}/${filename}`, response)
+			return response
+		} else {
+			return new Response(res.statusText, { status: res.status })
+		}
 	}
 }
 
@@ -77,10 +84,16 @@ export async function update(gistId: string, files: Record<string,string>) {
 	let input = {
 		files: {} as Record<string, {content: string}>
 	}
+	let cacheSets: (()=> Promise<any>)[] = [];
 	Object.keys(files).map(file => {
 		input.files[file] = {
 			content: files[file]
 		}
+		cacheSets.push(() => {
+			let cachedResponse = new Response(files[file]);
+			cachedResponse.headers.append('Cache-Control', 's-maxage=300')
+			return caches.default.put(`https://ghgist.local/${gistId}/${file}`, cachedResponse)
+		})
 	})
 	const content = JSON.stringify(input)
 	const res = await fetch(`https://api.github.com/gists/${gistId}`, {
@@ -93,5 +106,6 @@ export async function update(gistId: string, files: Record<string,string>) {
 		},
 		body: content
 	})
+	await Promise.all(cacheSets.map(f => f()))
 	return new Response(res.statusText, { status: res.status })
 }
